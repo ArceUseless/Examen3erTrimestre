95bf3ecccac5452d5253653ae6b0dc7bc66448a1
rafa245 <36188730+rafa245@users.noreply.github.com>
2019-06-11 12:27:51 +0200

    Add files via upload

--- exMayo2019RafaelJesúsNietoCardador/Ficheros.java
+++ exMayo2019RafaelJesúsNietoCardador/Ficheros.java
@@ -10,9 +10,12 @@ import javax.swing.JOptionPane;
 import java.awt.Font;
 import java.awt.Color;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -20,12 +23,23 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
+import javax.swing.JFileChooser;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+
 
 public class Ficheros {
 
   private JFrame frmCorrompeficheros;
-  private JTextField textField;
-  private JTextField textField_1;
+  private JTextField ficheroOrigen;
+  private JTextField ficheroDestino;
+  private JButton btnNewButton_2;
+  
+  private File fOrigen;
+  private File fDestino;
+  private JScrollPane scrollPane;
 
   /**
    * Launch the application.
@@ -58,21 +72,59 @@ public class Ficheros {
     frmCorrompeficheros.setResizable(false);
     frmCorrompeficheros.getContentPane().setBackground(new Color(0, 0, 0));
     frmCorrompeficheros.setTitle("Corrompe-Ficheros");
-    frmCorrompeficheros.setBounds(100, 100, 631, 349);
+    frmCorrompeficheros.setBounds(100, 100, 652, 593);
     frmCorrompeficheros.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frmCorrompeficheros.getContentPane().setLayout(null);
     
-    textField = new JTextField();
-    textField.setBackground(new Color(153, 153, 153));
-    textField.setBounds(35, 68, 223, 20);
-    frmCorrompeficheros.getContentPane().add(textField);
-    textField.setColumns(10);
+    ficheroOrigen = new JTextField();
+    ficheroOrigen.setEditable(false);
+    ficheroOrigen.setBackground(new Color(153, 153, 153));
+    ficheroOrigen.setBounds(18, 99, 242, 20);
+    frmCorrompeficheros.getContentPane().add(ficheroOrigen);
+    ficheroOrigen.setColumns(10);
     
-    JLabel lblIntroduzcaElNombre = new JLabel("Introduzca el nombre del fichero a corromper");
+    JLabel lblIntroduzcaElNombre = new JLabel("Ruta del fichero de origen");
     lblIntroduzcaElNombre.setForeground(new Color(0, 204, 0));
     lblIntroduzcaElNombre.setFont(new Font("Tahoma", Font.BOLD, 12));
-    lblIntroduzcaElNombre.setBounds(10, 36, 285, 20);
+    lblIntroduzcaElNombre.setBounds(55, 27, 173, 20);
     frmCorrompeficheros.getContentPane().add(lblIntroduzcaElNombre);
+    
+    ficheroDestino = new JTextField();
+    ficheroDestino.setEditable(false);
+    ficheroDestino.setColumns(10);
+    ficheroDestino.setBackground(new Color(153, 153, 153));
+    ficheroDestino.setBounds(384, 99, 242, 20);
+    frmCorrompeficheros.getContentPane().add(ficheroDestino);
+    
+    btnNewButton_2 = new JButton("Seleccionar archivo");
+    btnNewButton_2.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        JFileChooser fileChooser = new JFileChooser();
+        int fileC = fileChooser.showOpenDialog(frmCorrompeficheros);
+        if (fileC == JFileChooser.APPROVE_OPTION) {
+          fDestino = fileChooser.getSelectedFile();
+          ficheroDestino.setText(fDestino.getAbsolutePath());
+        }
+      }
+    });
+    btnNewButton_2.setBounds(431, 59, 153, 28);
+    frmCorrompeficheros.getContentPane().add(btnNewButton_2);
+    
+    JLabel lblRutaDelFichero = new JLabel("Ruta del fichero de destino");
+    lblRutaDelFichero.setForeground(new Color(0, 204, 0));
+    lblRutaDelFichero.setFont(new Font("Tahoma", Font.BOLD, 12));
+    lblRutaDelFichero.setBounds(411, 27, 229, 20);
+    frmCorrompeficheros.getContentPane().add(lblRutaDelFichero);
+    
+    scrollPane = new JScrollPane();
+    scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+    scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+    scrollPane.setBounds(31, 242, 595, 307);
+    frmCorrompeficheros.getContentPane().add(scrollPane);
+    
+    JTextArea textArea = new JTextArea();
+    textArea.setEditable(false);
+    scrollPane.setViewportView(textArea);
     
     JButton btnNewButton = new JButton("C0RR0MP3R");
     btnNewButton.setForeground(new Color(0, 51, 0));
@@ -80,73 +132,72 @@ public class Ficheros {
     btnNewButton.setFont(new Font("Franklin Gothic Medium", Font.BOLD, 18));
     btnNewButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent arg0) {
-        String nombreFichero = textField.getText();
-        
-        corrompeFichero(nombreFichero);
+        try {
+          String contenidoDocumentoC = "";
+          BufferedReader br = new BufferedReader(new FileReader(ficheroOrigen.getText()));
+          
+          ArrayList<String> contenidoFichero= new ArrayList<String>();
+
+          String linea = "";
+          while (linea != null) {
+            linea = br.readLine();
+            if (linea != null) {
+              contenidoFichero.add(linea);
+            }
+          }
+          br.close();
+          
+          BufferedWriter bw = new BufferedWriter(new FileWriter(ficheroDestino.getText()));
+          linea = "";
+          for(int i = 0; i < contenidoFichero.size(); i++) {
+            bw.write(contenidoFichero.get(i).replace('A', '4').replace('a', '4').replace('B', '8').replace('b', '8').replace('E', '3').replace('I', '1').replace('i', '1').replace('O', '0').replace('o', '0').replace('S', '5').replace('s', '5').replace('T', '7').replace('t', '7'));  
+            bw.newLine();
+          }
+          
+          bw.close();
+          
+          BufferedReader br2 = new BufferedReader(new FileReader(ficheroDestino.getText()));
+          
+          while (linea != null) {
+            linea = br2.readLine();
+            if (linea != null) {
+              contenidoDocumentoC = contenidoDocumentoC + linea + "\n";
+            }
+          }
+          textArea.append(contenidoDocumentoC);
+          
+          br2.close();
+          
+          JOptionPane.showMessageDialog(null, "F1ch3r0 c0rr0mp1d0 c0rr3ct4m3nt3.");
+
+        } catch (FileNotFoundException e) {
+          JOptionPane.showMessageDialog(null, "Error, fichero no encontrado.");
+        } catch (IOException e) {
+          JOptionPane.showMessageDialog(null, "Error al leer el fichero.");
+        } catch (Exception e) {
+          JOptionPane.showMessageDialog(null, "Error en la entrada de datos.");
+        }
       }
     });
-    btnNewButton.setBounds(46, 234, 202, 59);
+    btnNewButton.setBounds(225, 164, 202, 59);
     frmCorrompeficheros.getContentPane().add(btnNewButton);
     
-    textField_1 = new JTextField();
-    textField_1.setHorizontalAlignment(SwingConstants.LEFT);
-    textField_1.setEditable(false);
-    textField_1.setBackground(new Color(255, 255, 255));
-    textField_1.setBounds(307, 23, 294, 256);
-    frmCorrompeficheros.getContentPane().add(textField_1);
-    textField_1.setColumns(10);
+    JButton btnNewButton_1 = new JButton("Seleccionar archivo");
+    btnNewButton_1.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent arg0) {
+        JFileChooser fileChooser = new JFileChooser();
+        int fileC = fileChooser.showOpenDialog(frmCorrompeficheros);
+        if (fileC == JFileChooser.APPROVE_OPTION) {
+          fOrigen = fileChooser.getSelectedFile();
+          ficheroOrigen.setText(fOrigen.getAbsolutePath());
+        }
+      }
+      
+    });
+    btnNewButton_1.setBounds(69, 59, 148, 28);
+    frmCorrompeficheros.getContentPane().add(btnNewButton_1);
+    
+   
   }
   
-  public void corrompeFichero(String nombreFichero) {
-    try {
-      BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\d17nicar\\eclipse-workspace\\Programacion2\\exMayo2019RafaelJesúsNietoCardador\\src\\exMayo2019RafaelJesúsNietoCardador\\Ficheros\\"
-              + nombreFichero + ".txt"));
-      
-      ArrayList<String> contenidoFichero= new ArrayList<String>();
-
-      String linea = "";
-      while (linea != null) {
-        linea = br.readLine();
-        if (linea != null) {
-          contenidoFichero.add(linea);
-        }
-      }
-      br.close();
-      
-      String nombreFicheroC = nombreFichero.replace('A', '4').replace('a', '4').replace('B', '8').replace('b', '8').replace('E', '3').replace('I', '1').replace('i', '1').replace('O', '0').replace('o', '0').replace('S', '5').replace('s', '5').replace('T', '7').replace('t', '7');
-      
-      BufferedWriter bw = new BufferedWriter(new FileWriter( "C:\\Users\\d17nicar\\eclipse-workspace\\Programacion2\\exMayo2019RafaelJesúsNietoCardador\\src\\exMayo2019RafaelJesúsNietoCardador\\Ficheros\\"
-          + nombreFicheroC+"C.txt"));
-      linea = "";
-      for(int i = 0; i < contenidoFichero.size(); i++) {
-        bw.write(contenidoFichero.get(i).replace('A', '4').replace('a', '4').replace('B', '8').replace('b', '8').replace('E', '3').replace('I', '1').replace('i', '1').replace('O', '0').replace('o', '0').replace('S', '5').replace('s', '5').replace('T', '7').replace('t', '7'));  
-        bw.newLine();
-      }
-      
-      bw.close();
-      
-      BufferedReader br2 = new BufferedReader(new FileReader("C:\\Users\\d17nicar\\eclipse-workspace\\Programacion2\\exMayo2019RafaelJesúsNietoCardador\\src\\exMayo2019RafaelJesúsNietoCardador\\Ficheros\\"
-          + nombreFicheroC+"C.txt"));
-      
-      String contenidoDocumentoC = "";
-      while (linea != null) {
-        linea = br2.readLine();
-        if (linea != null) {
-          contenidoDocumentoC = contenidoDocumentoC + linea + "\n";
-        }
-      }
-      textField_1.setText(contenidoDocumentoC);
-      
-      br2.close();
-      
-      JOptionPane.showMessageDialog(null, "F1ch3r0 c0rr0mp1d0 c0rr3ct4m3nt3.");
-
-    } catch (FileNotFoundException e) {
-      JOptionPane.showMessageDialog(null, "Error, fichero no encontrado.");
-    } catch (IOException e) {
-      JOptionPane.showMessageDialog(null, "Error al leer el fichero.");
-    } catch (Exception e) {
-      JOptionPane.showMessageDialog(null, "Error en la entrada de datos.");
-    }
-  }
 }
